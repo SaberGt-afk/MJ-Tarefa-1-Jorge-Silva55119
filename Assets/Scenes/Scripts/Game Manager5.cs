c2ba998f4ea801e8c49e2854a78973a0e5a9bfe8
SaberGt-afk <gtsaber0000@gmail.com>
2025-05-23 10:43:31 +0100

    Lesson 5.3 - Game Over
    1. Create a Game Over text object/2. Make GameOver text appear/3. Create GameOver function/4. Stop spawning and score on GameOver/5. Add a Restart button/6. Make the restart button work/7. Show restart button on game over/8. Lesson Recap/

--- Assets/Scenes/Scripts/Game Manager5.cs
+++ Assets/Scenes/Scripts/Game Manager5.cs
@@ -2,25 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager5 : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
-    
+
     private int score;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
 
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
+
     }
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -35,7 +42,22 @@ public class GameManager5 : MonoBehaviour
         scoreText.text = "Score: " + score;
 
     }
+
+    public void GameOver()
+    {
+        
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+    public void RestartGame()
+    {
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
+
+
 
 
 
