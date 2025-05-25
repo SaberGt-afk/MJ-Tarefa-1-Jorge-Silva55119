bd9e242c390abd4350c90df54eb58301a8666c71
SaberGt-afk <gtsaber0000@gmail.com>
2025-05-25 11:25:50 +0100

    Challenge 5 - Whack-a-Food
    1. Overview/2. Warning/3. The difficulty buttons look messy/4. The food is being destroyed too soon/5. The Score is being replaced by the word “score”/6. When you lose, there’s no way to Restart/7. The difficulty buttons don’t change the difficulty/8. Bonus: The game can go on forever/9. Hints/10. Want even more challenges?

--- Assets/Scenes/Scripts/Target5.cs
+++ Assets/Scenes/Scripts/Target5.cs
@@ -6,7 +6,7 @@ public class Target5 : MonoBehaviour
     private GameManager5 gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
-    private float maxTorque = 10;
+    private float maxTorque = 20;
     private float xRange = 4;
     private float ySpawnPos = -6;
     public int pointValue;
